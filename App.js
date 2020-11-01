821cdabca875793e4bc7d70738a6f025f51c948b
MaxSven <maxence.svensson@epitech.eu>
2020-11-01 15:24:57 +0100

    Add icon

--- App.js
+++ App.js
@@ -1,70 +1,65 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from "./src/SplashScreen";
+import SearchBar from "./src/SearchBar"
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import SearchBar from "./src/SearchBar.js";
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <SearchBar/>
-      </SafeAreaView>
-    </>
-  );
-};
+    this.state = { isLoading: true }
+  }
+
+  performTimeConsumingTask = async() => {
+    return new Promise((resolve) =>
+        setTimeout(
+            () => { resolve('result') },
+            2000
+        )
+    );
+  }
+
+  async componentDidMount() {
+    // Preload data from an external API
+    // Preload data using AsyncStorage
+    const data = await this.performTimeConsumingTask();
+
+    if (data !== null) {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <SplashScreen />;
+    }
+
+    return (
+        <View style={styles.container}>
+          <SearchBar/>
+          <Text style={styles.welcome}>
+            Welcome to GitXplorer
+          </Text>
+        </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
-
-export default App;
---
+++ src/SplashScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Image, StyleSheet, Text, View} from "react-native";
+import LinearGradient from 'react-native-linear-gradient'
+
+export default class SplashScreen extends React.Component {
+    render() {
+        const viewStyles = [styles.container];
+        const textStyles = {
+            color: 'black',
+            fontSize: 40,
+            fontFamily: "JetBrainsMono-Regular"
+        };
+
+        return (
+            <View style={viewStyles}>
+                <Image
+                    style={styles.tinyLogo}
+                    source={require('../images/github-icon.png')}
+                />
+                <Text style={textStyles}>
+                    GitXplorer
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
